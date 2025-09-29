3b1e29bf3b6005b8b266e3b45a1217c60439562a
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-29 18:54:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,26 @@ function draufklickne() {
 let audio=document.querySelector("audio");
 let synth=new Tone.Synth();
 const actx = Tone.context;
+let dest=actx.createMediaStreamDestination();
+const recorder = new MediaRecorder(dest.stream);
+synth.connect(dest);
 synth.toDestination();
+let chunks=[];
 let notes="CDEFGAB".split("").map(n=>`${n}4`);
 let note=0;
 Tone.Transport.scheduleRepeat(time=>{
-  synth.triggerAttack(notes[note%notes.length]);
-  note++;},'4n');
-  
+  if(note==0){recorder.start();}
+  if(note>notes.length){
+    synth.triggerRelease(time);
+    recorder.stop();
+    Tone.Transport.stop();
+  } else {
+  synth.triggerAttack(notes[note],time);
+  note++;}
+  },'4n');
+recorder.ondataavailable=evt.chunks.push(evt.data);
+recorder.onstop=evt=>{
+  let blob=new Blob(chunks,{type:'audio/ogg; codecs=opus');
+  audio.src=URL.createObjectURL(blob);
+}
+Tone.Transport.start();
