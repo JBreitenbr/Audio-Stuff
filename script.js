d3b31b00b1487099695165890d8e2705425d508d
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-29 19:31:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-let hd=document.getElementById("hd");
-function draufklickne() {
-  hd.innerText="Let's play some music";
-}
+// UPDATE: there is a problem in chrome with starting audio context
+//  before a user gesture. This fixes it.
+var started = false;
+document.documentElement.addEventListener('mousedown', () => {
+  if (started) return;
+  started = true;
+  const audio = document.querySelector('audio');
+  const synth = new Tone.Synth();
+  const actx  = Tone.context;
+  const dest  = actx.createMediaStreamDestination();
+  const recorder = new MediaRecorder(dest.stream);
 
-let audio=document.querySelector("audio");
-let synth=new Tone.Synth();
-const actx = Tone.context;
-let dest=actx.createMediaStreamDestination();
-const recorder = new MediaRecorder(dest.stream);
-synth.connect(dest);
-synth.toDestination();
-let chunks=[];
-let notes="CDEFGAB".split("").map(n=>`${n}4`);
-let note=0;
-Tone.Transport.scheduleRepeat(time=>{
-  if(note==0){recorder.start();}
-  if(note>notes.length){
-    synth.triggerRelease(time);
-    recorder.stop();
-    Tone.Transport.stop();
-  } else {
-  synth.triggerAttack(notes[note],time);
-  note++;}
-  },'4n');
-recorder.ondataavailable=evt.chunks.push(evt.data);
-recorder.onstop=evt=>{
-  let blob=new Blob(chunks,{type:'audio/ogg; codecs=opus');
-  audio.src=URL.createObjectURL(blob);
-}
-Tone.Transport.start();
+  synth.connect(dest);
+  synth.toMaster();
+
+  const chunks = [];
+
+  const notes = 'CDEFGAB'.split('').map(n => `${n}4`);
+  let note = 0;
+  Tone.Transport.scheduleRepeat(time => {
+    if (note === 0) recorder.start();
+    if (note > notes.length) {
+      synth.triggerRelease(time)
+      recorder.stop();
+      Tone.Transport.stop();
+    } else synth.triggerAttack(notes[note], time);
+    note++;
+  }, '4n');
+
+  recorder.ondataavailable = evt => chunks.push(evt.data);
+  recorder.onstop = evt => {
+    let blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
+    audio.src = URL.createObjectURL(blob);
+  };
+
+  Tone.Transport.start();
+});
+
+
+  
+
+
