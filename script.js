4e630c5fcac1e79f91333c96db39c73819da9adf
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-29 18:40:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let hd=document.getElementById("hd");
-function draufklickne() => {
+function draufklickne() {
   hd.innerText="Let's play some music";
 }
+
+let audio=document.querySelector("audio");
+let synth=new Tone.Synth();
+const actx = Tone.context;
+synth.toDestination();
+let notes="CDEFGAB".split("").map(n=>`${n}4`);
+let note=0;
+Tone.Transport.scheduleRepeat(time=>{
+  synth.triggerAttack(notes[note%notes.length]);
+  note++;},'4n');
+  
